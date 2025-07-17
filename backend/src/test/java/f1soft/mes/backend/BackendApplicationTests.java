fd83819a76002338060333174bd669ded56c77a9
김기형 <socra710@gmail.com>
2025-07-17 13:40:26 +0900

    프로젝트 초기 커밋

---
+++ backend/src/test/java/f1soft/mes/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package f1soft.mes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.out.println(("안녕하세요."));
+    }
+
+}
