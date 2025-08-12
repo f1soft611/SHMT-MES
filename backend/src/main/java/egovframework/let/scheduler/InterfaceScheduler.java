319201260d1890ac0294d00113d5ef8a0f35bc2d
김기형 <socra710@gmail.com>
2025-08-12 13:40:32 +0900

    인터페이스 로그 상세 추가중

--- backend/src/main/java/egovframework/let/scheduler/InterfaceScheduler.java
+++ backend/src/main/java/egovframework/let/scheduler/InterfaceScheduler.java
@@ -11,7 +11,7 @@ public class InterfaceScheduler {
 
     private final ErpToMesInterfaceService erpToMesInterfaceService;
 
-    @Scheduled(cron = "0 * * * * *") // 매시간 0분마다 실행
+    @Scheduled(cron = "0 0 * * * *") // 매시간 0분마다 실행
     public void runInterfaceJob() {
         // 서비스 호출 (예: 이력 등록, 조회 등)
         try {
